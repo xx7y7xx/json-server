0a29a1868b18ea0e3fc4223eefeb3eafffd9d657
Devin Chen <d3vin.chen@gmail.com>
2017-07-19 20:39:37 +0800

    fix EADDRINUSE error

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ const Koa = require('koa');
 // const app = new Koa();
 const app = module.exports = new Koa();
 
+// x-response-time
+
+app.use(async function(ctx, next) {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// logger
+
+app.use(async function(ctx, next) {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+// response
+
 app.use(ctx => {
   ctx.body = 'Hello World';
 });
 
-app.listen(3000);
+if (!module.parent) app.listen(3000);
